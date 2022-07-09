cb638970a466350e16e4087aff0df3312b273cf0
Oscar J Rodriguez <josuer08@gmail.com>
2022-07-09 14:31:51 -0400

    Usage of more standar GNU/bash (sd > tr + sed)
    The changes implemented here are just so everyone can run this script without installing extra packages

--- bin/crowdnode-list-apis.sh
+++ bin/crowdnode-list-apis.sh
@@ -3,9 +3,9 @@ set -e
 set -u
 
 curl 'https://knowledge.crowdnode.io/en/articles/5963880-blockchain-api-guide' |
-    sd '>' '\n' |
-    sd '<' '\n' |
-    grep '\<https://app\.crowdnode\.io' |
+    tr '>' '\n' |
+    tr '<' '\n' |
+    grep 'https://app.crowdnode.io' |
     grep -v 'href=' |
-    sd -s '[YOUR_ADDRESS]' '${pub}' |
-    sd -s '[ADDRESS]' '${pub}'
+    sed 's/\[YOUR_ADDRESS\]/${pub}/' |
+    sed 's/\[ADDRESS\]/${pub}/'
